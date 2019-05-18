8f55d51db59feb26ed6f1e7ffa2944e55ef59a8e
zephyrMo <343996275@qq.com>
2019-05-18 17:04:27 +0800

    config again

---
+++ src/pages/products.jsx
@@ -0,0 +1,10 @@
+
+import styles from './products.css';
+
+export default function () {
+  return (
+    <div className={styles.normal}>
+      <h1 className={styles.h1}>Page products1231231231</h1>
+    </div>
+  );
+}
